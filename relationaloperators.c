7159f6d022a0afb5390980ef8b3bdac5bdd17cbc
Pala Shreya <shreyapala77@gmail.com>
2025-11-11 23:28:06 +0530

    Create relationaloperators.c

---
+++ relationaloperators.c
@@ -0,0 +1,15 @@
+//relational operators
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter two numbers:");
+	scanf("%d%d",&a,&b);
+	printf("%d<%d=%d\n",a,b,a<b);
+	printf("%d>%d=%d\n",a,b,a>b);
+	printf("%d<=%d=%d\n",a,b,a<=b);
+	printf("%d>=%d=%d\n",a,b,a>=b);
+	printf("%d==%d=%d\n",a,b,a==b);
+	printf("%d!=%d=%d\n",a,b,a!=b);
+	return 0;
+}
