1d12dcd29c6ccf1cf5327f2ba6bd8f4f195bf6ff
Pala Shreya <shreyapala77@gmail.com>
2025-11-11 22:45:05 +0530

    Create power(x,n).c

---
+++ power(x,n).c
@@ -0,0 +1,13 @@
+//write a c program to find x^n
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int b,e;
+	printf("enter base:");
+	scanf("%d",&b);
+	printf("enter exponent:");
+	scanf("%d",&e);
+	printf("pow(%d,%d)=%f\n",b,e,pow(b,e));
+	return 0;
+}
