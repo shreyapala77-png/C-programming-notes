0d6221e5803f3654a059d1eda875bb35a8c6334c
Pala Shreya <shreyapala77@gmail.com>
2025-11-11 22:55:40 +0530

    Create squarerootofanumber.c

---
+++ squarerootofanumber.c
@@ -0,0 +1,12 @@
+//write a c program to find square root of a given number
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,sr;
+	printf("enter a number:");
+	scanf("%d",&n);
+	sr=sqrt(n);
+	printf("%d",sr);
+	return 0;
+}
